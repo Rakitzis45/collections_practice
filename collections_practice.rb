9d1b65feadd1415bd7c1b161cfe0330f0fccb53e
Rakitzis45 <michael.rakitzis@gmail.com>
2020-10-02 14:21:10 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+    array.sort{|a,b| a <=> b}
+end
 
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort{|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    return array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each{|kesha| kesha[2]= "$"}
+end
+
+def find_a(array)
+    array.select{|words| words[0] == "a"}
+end
+
+def sum_array(array)
+    sum = 0 
+    array.each{|n| sum += n}
+    return sum
+end
+
+def add_s(array)
+    array.map do |words| 
+        if array[1] == words
+            words
+        else
+            words + "s"
+        end
+    end
+end
